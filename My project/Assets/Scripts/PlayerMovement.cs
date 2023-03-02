de1c6a54cfadb6480105152afa4b2c31dbc7136f
Apollo <jackratermann@gmail.com>
2023-03-01 20:41:53 -0800

    Climbing, Jumping, More
    Added jumping, climbing with logic to play animations at correct timing. Edited ladder collider to ensure player isn't hanging on by a fingernail when climbing. Also ensuring the climbing animation plays when climbing the ladder and doesn't play when touching the ground and attempting to climb downwards.

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -9,11 +9,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float playerSpeed = 5f;
     [SerializeField] Animator pAnim;
+    [SerializeField] float jumpSpeed = 7f;
+    CapsuleCollider2D pColl;
+    [SerializeField] float climbSpeed = 5f;
+    float playerGravAtStart;
     // Start is called before the first frame update
     void Start()
     {
+        pColl = GetComponent<CapsuleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
         pAnim = GetComponent<Animator>();
+        playerGravAtStart = rb.gravityScale;
     }
 
     // Update is called once per frame
@@ -21,10 +27,18 @@ public class PlayerMovement : MonoBehaviour
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value){
         moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value){
+        if(!pColl.IsTouchingLayers(LayerMask.GetMask("Ground"))){ return; }
+        if(value.isPressed){
+            rb.velocity += new Vector2 (0f, jumpSpeed);
+        }
     }
 
     void Run(){
@@ -42,5 +56,20 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector2 (Mathf.Sign(rb.velocity.x), 1f);
         }
         
+    }
+
+    void ClimbLadder(){
+        if(!pColl.IsTouchingLayers(LayerMask.GetMask("Climbing"))){
+            rb.gravityScale = playerGravAtStart;
+            return;
+        }
+
+        Vector2 climbVelocity = new Vector2 (rb.velocity.x, moveInput.y * climbSpeed);
+        rb.velocity = climbVelocity;
+        rb.gravityScale = 0f;
+
+        if(Mathf.Abs(rb.velocity.y) > Mathf.Epsilon && !pColl.IsTouchingLayers(LayerMask.GetMask("Ground"))){
+            pAnim.SetBool("isClimbing", true);
+        } else pAnim.SetBool("isClimbing", false);
     }
 }
