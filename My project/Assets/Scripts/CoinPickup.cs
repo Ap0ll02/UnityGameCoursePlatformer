b46b05ffaf0c2446f3d5f76ccd221300abb6f1e6
Apollo <jackratermann@gmail.com>
2023-03-20 00:52:25 -0700

    Extra Devs
    I hope this update works, if not,
    Camera Stuff

--- My project/Assets/Scripts/CoinPickup.cs
+++ My project/Assets/Scripts/CoinPickup.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip myAud;
+    //[SerializeField] GameObject cam;
     int Score;
     void OnTriggerEnter2D(Collider2D other) {
-
         if(other.tag == "Player"){
-            AudioSource.PlayClipAtPoint(myAud, cam.main.transform.postion);
+            AudioSource.PlayClipAtPoint(myAud, Camera.main.transform.position);
             Destroy(gameObject);
         }
     }
